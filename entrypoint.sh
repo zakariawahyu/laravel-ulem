b7d43ecc6e8569f52976e9a1ea5a182382a49c87
zakariawahyu <zakarianur6@gmail.com>
2024-10-07 17:03:04 +0700

    (deploy) Configuration Deployment CI/CD

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker compose exec app php artisan migrate
+docker compose exec app php artisan db:seed
+docker compose exec app php artisan optimize:clear
+docker compose exec app php artisan optimize
+docker compose exec app php artisan package:discover --ansi
+docker compose exec app systemctl start supervisor
