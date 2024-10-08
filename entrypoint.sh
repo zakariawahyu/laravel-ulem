cb2183eed3e2adf0b3968474548c99b29b8e1a3a
zakariawahyu <zakarianur6@gmail.com>
2024-10-08 23:26:25 +0700

    (deploy) - Testing deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+docker compose exec app composer install
 docker compose exec app php artisan key:generate
-docker compose exec app php artisan migrate
-docker compose exec app php artisan db:seed
 docker compose exec app php artisan optimize:clear
 docker compose exec app php artisan optimize
 docker compose exec app php artisan package:discover --ansi
