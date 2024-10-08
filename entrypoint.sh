41c371bffbe6c5fdbed0e8d64213f570352cfbd2
zakariawahyu <zakarianur6@gmail.com>
2024-10-08 21:23:40 +0700

    (deploy) - Update deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker compose exec app php artisan key:generate
 docker compose exec app php artisan migrate
 docker compose exec app php artisan db:seed
 docker compose exec app php artisan optimize:clear
