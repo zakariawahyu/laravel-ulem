4e599ddd1ae3d3f4544212653f0c4f5d1ee87edc
zakariawahyu <zakarianur6@gmail.com>
2024-10-09 11:08:29 +0700

    (deploy) - Revert deployment and fix file upload

--- app/Libraries/UploadImage.php
+++ app/Libraries/UploadImage.php
@@ -10,8 +10,10 @@ class UploadImage {
         $file       = $request->file($fieldName);
         $imageName  = $caption.".".$file->getClientOriginalExtension();
 
-        $request->$fieldName->move(public_path('images'), $imageName);
+        $path = $request->file($field)->storeAs(
+            'images', $imageName
+        );
 
-        return $imageName;
+        return $path;
     }
 }
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker compose exec app composer install
 docker compose exec app php artisan key:generate
 docker compose exec app php artisan optimize:clear
 docker compose exec app php artisan optimize
